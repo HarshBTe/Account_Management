21ef2beb02db52d60b7439331ab679434faf9990
Harsh Dubey <harshdubey06003@gmail.com>
2025-01-26 17:24:47 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,10 @@ const userRoutes = require('./routes/user');
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Account Management API');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
